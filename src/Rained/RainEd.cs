58f28258329d08199ad876f3132852a259967c4b
pkhead <chromosoze@gmail.com>
2024-03-03 20:08:43 -0600

    change version number

--- src/Rained/RainEd.cs
+++ src/Rained/RainEd.cs
@@ -10,7 +10,7 @@ namespace RainEd;
 
 sealed class RainEd
 {
-    private const string VERSION = "dev"; 
+    private const string VERSION = "1.0.0"; 
 
     public static RainEd Instance = null!;
 
