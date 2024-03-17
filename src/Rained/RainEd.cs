2cb50cd47d4e40299339baf5fdd0e076a07f59fe
pkhead <chromosoze@gmail.com>
2024-03-16 23:30:47 -0500

    bump version to b1.2.1

--- src/Rained/RainEd.cs
+++ src/Rained/RainEd.cs
@@ -11,7 +11,7 @@ namespace RainEd;
 
 sealed class RainEd
 {
-    public const string Version = "b1.2.0"; 
+    public const string Version = "b1.2.1"; 
 
     public static RainEd Instance = null!;
 
