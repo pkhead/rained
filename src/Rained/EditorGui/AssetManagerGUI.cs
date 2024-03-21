9780dc883b2105275130f3d03c3e9025f3e53047
pkhead <chromosoze@gmail.com>
2024-03-21 17:17:05 -0500

    import buttons in asset manager gui

--- src/Rained/EditorGui/AssetManagerGUI.cs
+++ src/Rained/EditorGui/AssetManagerGUI.cs
@@ -20,7 +20,128 @@ static class AssetManagerGUI
     private static int groupIndex = 0;
     private static AssetManager assetManager = null!;
     private static bool needRestart = false;
-    
+
+    private static FileBrowser? fileBrowser = null;
+
+    // uncolored variant
+    private static void ShowCategoryList<T>(List<T> categories, ref int selected, Vector2 listSize)
+        where T : InitCategory
+    {
+        if (ImGui.BeginListBox("##Categories", listSize))
+        {
+            for (int i = 0; i < categories.Count; i++)
+            {
+                var group = categories[i];
+                var cursor = ImGui.GetCursorScreenPos();
+                if (ImGui.Selectable(group.Name, i == selected))
+                {
+                    if (selected != i)
+                        groupIndex = 0;
+                    
+                    selected = i;
+                }
+            }
+
+            ImGui.EndListBox();
+        }
+    }
+
+    // colored variant
+    private static void ShowColoredCategoryList(List<ColoredInitCategory> categories, ref int selected, Vector2 listSize)
+    {
+        if (ImGui.BeginListBox("##Categories", listSize))
+        {
+            var drawList = ImGui.GetWindowDrawList();
+            float textHeight = ImGui.GetTextLineHeight();
+
+            for (int i = 0; i < categories.Count; i++)
+            {
+                var group = categories[i];
+                var cursor = ImGui.GetCursorScreenPos();
+                if (ImGui.Selectable("  " + group.Name, i == selected))
+                {
+                    if (selected != i)
+                        groupIndex = 0;
+                    
+                    selected = i;
+                }
+
+                drawList.AddRectFilled(
+                    p_min: cursor,
+                    p_max: cursor + new Vector2(10f, textHeight),
+                    ImGui.ColorConvertFloat4ToU32(new Vector4(group.Color.R / 255f, group.Color.G / 255f, group.Color.B / 255, 1f))
+                );
+            }
+
+            ImGui.EndListBox();
+        }
+    }
+
+    private static void ShowItemList<T>(List<T> categories, int selected, Vector2 listSize)
+        where T : ColoredInitCategory
+    {
+        // group listing list box
+        ImGui.SameLine();
+        if (ImGui.BeginListBox("##Items", listSize))
+        {
+            var itemList = categories[selected].Items;
+
+            for (int i = 0; i < itemList.Count; i++)
+            {
+                var tile = itemList[i];
+
+                // don't show this prop if it doesn't pass search test
+                //if (!tile.Name.Contains(searchQuery, StringComparison.CurrentCultureIgnoreCase))
+                //    continue;
+                
+                if (ImGui.Selectable(tile.Name, i == groupIndex))
+                {
+                    groupIndex = i;
+                }
+            }
+
+            ImGui.EndListBox();
+        }
+    }
+
+    private static void AssetControls()
+    {
+        static bool isInitFile(string path, bool isRw)
+        {
+            return Path.GetFileName(path) == "Init.txt";
+        }
+
+        if (ImGui.Button("Import Init.txt"))
+        {
+            fileBrowser = new FileBrowser(FileBrowser.OpenMode.Read, ImportFile, RainEd.Instance.AssetDataPath);
+            fileBrowser.AddFilter("Init.txt File", isInitFile, ".txt");
+        }
+        
+        ImGui.SameLine();
+        if (ImGui.Button("Import .zip"))
+        {
+            fileBrowser = new FileBrowser(FileBrowser.OpenMode.Read, ImportZip, RainEd.Instance.AssetDataPath);
+            fileBrowser.AddFilter("ZIP File", null, ".zip");
+        }
+
+        // render file browser
+        FileBrowser.Render(ref fileBrowser);
+    }
+
+    private static void ImportFile(string? path)
+    {
+        if (path is null) return;
+
+        RainEd.Logger.Information("Import Init.txt file '{Path}'", path);
+    }
+
+    private static void ImportZip(string? path)
+    {
+        if (path is null) return;
+
+        RainEd.Logger.Information("Import zip file '{Path}'", path);
+    }
+
     public static void Show()
     {
         assetManager ??= new AssetManager();
@@ -46,93 +167,6 @@ static class AssetManagerGUI
             ImGui.BeginDisabled();
         }
 
-        if (ImGui.Button("Import Tiles"))
-        {
-
-        }
-        ImGui.SameLine();
-        ImGui.Button("Import Props");
-
-        // uncolored variant
-        void ShowCategoryList(List<InitCategory> categories, ref int selected, Vector2 listSize)
-        {
-            if (ImGui.BeginListBox("##Categories", listSize))
-            {
-                for (int i = 0; i < categories.Count; i++)
-                {
-                    var group = categories[i];
-                    var cursor = ImGui.GetCursorScreenPos();
-                    if (ImGui.Selectable(group.Name, i == selected))
-                    {
-                        if (selected != i)
-                            groupIndex = 0;
-                        
-                        selected = i;
-                    }
-                }
-
-                ImGui.EndListBox();
-            }
-        }
-
-        // colored variant
-        void ShowColoredCategoryList(List<ColoredInitCategory> categories, ref int selected, Vector2 listSize)
-        {
-            if (ImGui.BeginListBox("##Categories", listSize))
-            {
-                var drawList = ImGui.GetWindowDrawList();
-                float textHeight = ImGui.GetTextLineHeight();
-
-                for (int i = 0; i < categories.Count; i++)
-                {
-                    var group = categories[i];
-                    var cursor = ImGui.GetCursorScreenPos();
-                    if (ImGui.Selectable("  " + group.Name, i == selected))
-                    {
-                        if (selected != i)
-                            groupIndex = 0;
-                        
-                        selected = i;
-                    }
-
-                    drawList.AddRectFilled(
-                        p_min: cursor,
-                        p_max: cursor + new Vector2(10f, textHeight),
-                        ImGui.ColorConvertFloat4ToU32(new Vector4(group.Color.R / 255f, group.Color.G / 255f, group.Color.B / 255, 1f))
-                    );
-                }
-
-                ImGui.EndListBox();
-            }
-        }
-
-        void ShowItemList<T>(List<T> categories, int selected, Vector2 listSize)
-            where T : ColoredInitCategory
-        {
-            // group listing list box
-            ImGui.SameLine();
-            if (ImGui.BeginListBox("##Items", listSize))
-            {
-                var itemList = categories[selected].Items;
-
-                for (int i = 0; i < itemList.Count; i++)
-                {
-                    var tile = itemList[i];
-
-                    // don't show this prop if it doesn't pass search test
-                    //if (!tile.Name.Contains(searchQuery, StringComparison.CurrentCultureIgnoreCase))
-                    //    continue;
-                    
-                    if (ImGui.Selectable(tile.Name, i == groupIndex))
-                    {
-                        groupIndex = i;
-                    }
-                }
-
-                ImGui.EndListBox();
-            }
-        }
-
         // show tile database
         if (ImGui.BeginTabBar("AssetType"))
         {
@@ -145,6 +179,7 @@ static class AssetManagerGUI
                     curAssetTab = AssetType.Tile;
                 }
 
+                AssetControls();
                 var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
                 var boxHeight = ImGui.GetContentRegionAvail().Y;
 
@@ -163,6 +198,7 @@ static class AssetManagerGUI
                     curAssetTab = AssetType.Prop;
                 }
 
+                AssetControls();
                 var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
                 var boxHeight = ImGui.GetContentRegionAvail().Y;
 
@@ -181,6 +217,7 @@ static class AssetManagerGUI
                     curAssetTab = AssetType.Material;
                 }
 
+                AssetControls();
                 var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
                 var boxHeight = ImGui.GetContentRegionAvail().Y;
 
