b8556d60d2c97976591e215d99f3cd5ce84edf10
pkhead <chromosoze@gmail.com>
2024-03-21 16:51:21 -0500

    asset manager read props and materials

--- src/Rained/AssetDatabase/AssetManager.cs
+++ src/Rained/AssetDatabase/AssetManager.cs
@@ -16,17 +16,15 @@
 
         5. Stores the line numbers. I guess. And the actual string.
 */
-using Raylib_cs;
-
 namespace RainEd.Assets;
 
-record TileInit
+record InitData
 {
     public string Name;
     public string RawData;
     public int LineNumber;
 
-    public TileInit(string name, string data, int line)
+    public InitData(string name, string data, int line)
     {
         Name = name;
         RawData = data;
@@ -34,33 +32,57 @@ record TileInit
     }
 }
 
-record TileInitCategory
+record InitCategory
 {
     public string Name;
-    public Lingo.Color Color;
-    public List<TileInit> Tiles;
+    public List<InitData> Items;
     public int LineNumber;
 
-    public TileInitCategory(string name, Lingo.Color color, int line)
+    public InitCategory(string name, int line)
+    {
+        Name = name;
+        LineNumber = line;
+        Items = [];
+    }
+}
+
+record ColoredInitCategory : InitCategory
+{
+    public Lingo.Color Color;
+
+    public ColoredInitCategory(string name, Lingo.Color color, int line) : base(name, line)
     {
         Name = name;
         Color = color;
-        Tiles = [];
+        Items = [];
         LineNumber = line;
     }
 }
 
+
 class AssetManager
 {
-    public List<TileInitCategory> TileInit;
+    public List<ColoredInitCategory> TileInit;
+    public List<ColoredInitCategory> PropInit;
+    public List<InitCategory> MaterialsInit;
 
     public AssetManager()
     {
         TileInit = [];
-        ParseTileInit(Path.Combine(RainEd.Instance.AssetDataPath, "Graphics", "Init.txt"), TileInit);
+        PropInit = [];
+        MaterialsInit = [];
+
+        // parse tile Init.txt
+        ParseColoredInit(Path.Combine(RainEd.Instance.AssetDataPath, "Graphics", "Init.txt"), TileInit);
+        
+        // parse prop Init.txt
+        ParseColoredInit(Path.Combine(RainEd.Instance.AssetDataPath, "Props", "Init.txt"), PropInit);
+
+        // parse materials Init.txt
+        ParseMaterialInit(Path.Combine(RainEd.Instance.AssetDataPath, "Materials", "Init.txt"), MaterialsInit);
     }
 
-    private static void ParseTileInit(string path, List<TileInitCategory> outputCategories)
+    private static void ParseColoredInit(string path, List<ColoredInitCategory> outputCategories)
     {
         var parser = new Lingo.LingoParser();
 
@@ -77,7 +99,7 @@ class AssetManager
             if (line[0] == '-')
             {
                 var list = (Lingo.List) parser.Read(line[1..])!;
-                outputCategories.Add(new TileInitCategory(
+                outputCategories.Add(new ColoredInitCategory(
                     name: (string) list.values[0],
                     color: (Lingo.Color) list.values[1],
                     line: lineNo
@@ -88,9 +110,51 @@ class AssetManager
             else
             {
                 var category = outputCategories[^1];
+                var data = parser.Read(line) as Lingo.List;
+
+                if (data is not null)
+                {
+                    category.Items.Add(new InitData(
+                        name: (string) data.fields["nm"],
+                        data: line,
+                        line: lineNo
+                    ));
+                }
+            }
+
+            lineNo++;
+        }
+    }
+
+    private static void ParseMaterialInit(string path, List<InitCategory> outputCategories)
+    {
+        var parser = new Lingo.LingoParser();
+
+        int lineNo = 0;
+        foreach (var line in File.ReadLines(path))
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                lineNo++;
+                continue;
+            }
+
+            // category header
+            if (line[0] == '-')
+            {
+                outputCategories.Add(new InitCategory(
+                    name: line[1..],
+                    line: lineNo
+                ));
+            }
+
+            // tile init
+            else
+            {
+                var category = outputCategories[^1];
                 var data = (Lingo.List) parser.Read(line)!;
 
-                category.Tiles.Add(new TileInit(
+                category.Items.Add(new InitData(
                     name: (string) data.fields["nm"],
                     data: line,
                     line: lineNo
--- src/Rained/EditorGui/PreferencesWindow.cs
+++ src/Rained/EditorGui/PreferencesWindow.cs
@@ -237,7 +237,14 @@ static class PreferencesWindow
         }
     }
 
+    enum AssetType
+    {
+        Tile, Prop, Material
+    }
+    private static AssetType curAssetTab = AssetType.Tile;
     private static int selectedTileCategory = 0;
+    private static int selectedPropCategory = 0;
+    private static int selectedMatCategory = 0;
     private static int groupIndex = 0;
     private static AssetManager assetManager = null!;
 
@@ -273,62 +280,139 @@ static class PreferencesWindow
         ImGui.SameLine();
         ImGui.Button("Import Props");
 
+        // uncolored variant
+        void ShowCategoryList(List<InitCategory> categories, ref int selected, Vector2 listSize)
+        {
+            if (ImGui.BeginListBox("##Categories", listSize))
+            {
+                for (int i = 0; i < categories.Count; i++)
+                {
+                    var group = categories[i];
+                    var cursor = ImGui.GetCursorScreenPos();
+                    if (ImGui.Selectable(group.Name, i == selected))
+                    {
+                        if (selected != i)
+                            groupIndex = 0;
+                        
+                        selected = i;
+                    }
+                }
+
+                ImGui.EndListBox();
+            }
+        }
+
+        // colored variant
+        void ShowColoredCategoryList(List<ColoredInitCategory> categories, ref int selected, Vector2 listSize)
+        {
+            if (ImGui.BeginListBox("##Categories", listSize))
+            {
+                var drawList = ImGui.GetWindowDrawList();
+                float textHeight = ImGui.GetTextLineHeight();
+
+                for (int i = 0; i < categories.Count; i++)
+                {
+                    var group = categories[i];
+                    var cursor = ImGui.GetCursorScreenPos();
+                    if (ImGui.Selectable("  " + group.Name, i == selected))
+                    {
+                        if (selected != i)
+                            groupIndex = 0;
+                        
+                        selected = i;
+                    }
+
+                    drawList.AddRectFilled(
+                        p_min: cursor,
+                        p_max: cursor + new Vector2(10f, textHeight),
+                        ImGui.ColorConvertFloat4ToU32(new Vector4(group.Color.R / 255f, group.Color.G / 255f, group.Color.B / 255, 1f))
+                    );
+                }
+
+                ImGui.EndListBox();
+            }
+        }
+
+        void ShowItemList<T>(List<T> categories, int selected, Vector2 listSize)
+            where T : ColoredInitCategory
+        {
+            // group listing list box
+            ImGui.SameLine();
+            if (ImGui.BeginListBox("##Items", listSize))
+            {
+                var itemList = categories[selected].Items;
+
+                for (int i = 0; i < itemList.Count; i++)
+                {
+                    var tile = itemList[i];
+
+                    // don't show this prop if it doesn't pass search test
+                    //if (!tile.Name.Contains(searchQuery, StringComparison.CurrentCultureIgnoreCase))
+                    //    continue;
+                    
+                    if (ImGui.Selectable(tile.Name, i == groupIndex))
+                    {
+                        groupIndex = i;
+                    }
+                }
+
+                ImGui.EndListBox();
+            }
+        }
+
         // show tile database
         if (ImGui.BeginTabBar("AssetType"))
         {
             if (ImGui.BeginTabItem("Tiles"))
             {
+                // set group index to 0 when tab changed
+                if (curAssetTab != AssetType.Tile)
+                {
+                    groupIndex = 0;
+                    curAssetTab = AssetType.Tile;
+                }
+
                 var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
                 var boxHeight = ImGui.GetContentRegionAvail().Y;
-                if (ImGui.BeginListBox("##Categories", new Vector2(halfWidth, boxHeight)))
+
+                ShowColoredCategoryList(assetManager.TileInit, ref selectedTileCategory, new Vector2(halfWidth, boxHeight));
+                ShowItemList(assetManager.TileInit, selectedTileCategory, new Vector2(halfWidth, boxHeight));
+
+                ImGui.EndTabItem();
+            }
+
+            if (ImGui.BeginTabItem("Props"))
+            {
+                // set group index to 0 when tab changed
+                if (curAssetTab != AssetType.Prop)
                 {
-                    var drawList = ImGui.GetWindowDrawList();
-                    float textHeight = ImGui.GetTextLineHeight();
-
-                    for (int i = 0; i < assetManager.TileInit.Count; i++)
-                    {
-                        var group = assetManager.TileInit[i];
-                        var cursor = ImGui.GetCursorScreenPos();
-                        if (ImGui.Selectable("  " + group.Name, i == selectedTileCategory))
-                        {
-                            if (selectedTileCategory != i)
-                                groupIndex = 0;
-                            
-                            selectedTileCategory = i;
-                        }
-
-                        drawList.AddRectFilled(
-                            p_min: cursor,
-                            p_max: cursor + new Vector2(10f, textHeight),
-                            ImGui.ColorConvertFloat4ToU32(new Vector4(group.Color.R / 255f, group.Color.G / 255f, group.Color.B / 255, 1f))
-                        );
-                    }
-
-                    ImGui.EndListBox();
+                    groupIndex = 0;
+                    curAssetTab = AssetType.Prop;
                 }
 
-                // group listing list box
-                ImGui.SameLine();
-                if (ImGui.BeginListBox("##Tiles", new Vector2(halfWidth, boxHeight)))
+                var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
+                var boxHeight = ImGui.GetContentRegionAvail().Y;
+
+                ShowColoredCategoryList(assetManager.PropInit, ref selectedPropCategory, new Vector2(halfWidth, boxHeight));
+                ShowItemList(assetManager.PropInit, selectedPropCategory, new Vector2(halfWidth, boxHeight));
+
+                ImGui.EndTabItem();
+            }
+
+            if (ImGui.BeginTabItem("Materials"))
+            {
+                // set group index to 0 when tab changed
+                if (curAssetTab != AssetType.Material)
                 {
-                    var tileList = assetManager.TileInit[selectedTileCategory].Tiles;
-
-                    for (int i = 0; i < tileList.Count; i++)
-                    {
-                        var tile = tileList[i];
-
-                        // don't show this prop if it doesn't pass search test
-                        //if (!tile.Name.Contains(searchQuery, StringComparison.CurrentCultureIgnoreCase))
-                        //    continue;
-                        
-                        if (ImGui.Selectable(tile.Name, i == groupIndex))
-                        {
-                            groupIndex = i;
-                        }
-                    }
-
-                    ImGui.EndListBox();
+                    groupIndex = 0;
+                    curAssetTab = AssetType.Material;
                 }
+
+                var halfWidth = ImGui.GetContentRegionAvail().X / 2f - ImGui.GetStyle().ItemSpacing.X / 2f;
+                var boxHeight = ImGui.GetContentRegionAvail().Y;
+
+                ShowCategoryList(assetManager.MaterialsInit, ref selectedMatCategory, new Vector2(halfWidth, boxHeight));
+                ShowItemList(assetManager.TileInit, selectedMatCategory, new Vector2(halfWidth, boxHeight));
 
                 ImGui.EndTabItem();
             }
