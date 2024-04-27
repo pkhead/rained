36efbbfee990cf672af044ede4ea9809905fa0d0
pkhead <chromosoze@gmail.com>
2024-04-27 12:38:58 -0500

    rect pattern boxes

--- scripts/autotiles/alt-grate.lua
+++ scripts/autotiles/alt-grate.lua
@@ -1,11 +1,42 @@
+local helpers = require("helpers")
+
 -- setup autotile data
-local autotile = rained.createAutotile("SU Grate Box")
+local autotile = rained.createAutotile("Alt Grate box")
 autotile.type = "rect"
 
 -- Rained will not allow the user to use this autotile
 -- if any of the tiles in this table are not installed
 autotile.requiredTiles = {
-    "empty"
+    "Block Corner NW",
+    "Block Corner NE",
+    "Block Corner SE",
+    "Block Corner SW",
+    "AltGrateA",
+    "AltGrateB1",
+    "AltGrateB2",
+    "AltGrateB3",
+    "AltGrateB4",
+    "AltGrateC1",
+    "AltGrateC2",
+    "AltGrateE1",
+    "AltGrateE2",
+    "AltGrateF1",
+    "AltGrateF2",
+    "AltGrateF1",
+    "AltGrateF2",
+    "AltGrateF3",
+    "AltGrateF4",
+    "AltGrateG1",
+    "AltGrateG2",
+    "AltGrateI",
+    "AltGrateJ1",
+    "AltGrateJ2",
+    "AltGrateJ1",
+    "AltGrateJ2",
+    "AltGrateJ3",
+    "AltGrateJ4",
+    "AltGrateJ3",
+    "AltGrateB1"
 }
 
 -- this is the callback function that Rained invokes when the user
@@ -17,9 +48,5 @@ autotile.requiredTiles = {
 ---@param bottom integer The Y coordinate of the bottom side of the rectangle.
 ---@param forceModifier ForceModifier Force-placement mode, as a string. Can be nil, "force", or "geometry".
 function autotile:tileRect(layer, left, top, right, bottom, forceModifier)
-    for x=left, right do
-        for y=top, bottom do
-            rained.placeTile("empty", layer, x, y, forceModifier)
-        end
-    end
+    helpers.patternBox("AltGrate", layer, left, top, right, bottom, forceModifier)
 end
--- scripts/helpers.lua
+++ scripts/helpers.lua
@@ -36,4 +36,90 @@ function module.autotilePath(tileTable, layer, segments, forceModifier, startInd
     end
 end
 
+-- this data is for the patternBox helper function.
+-- adapted from tileEditor.lingo
+local patterns = {
+    {tiles = {"A"}, upper = "dense", lower = "dense", tall = 1, freq = 5},
+    {tiles = {"B1"}, upper = "espaced", lower = "dense", tall = 1, freq = 5},
+    {tiles = {"B2"}, upper = "dense", lower = "espaced", tall = 1, freq = 5},
+    {tiles = {"B3"}, upper = "ospaced", lower = "dense", tall = 1, freq = 5},
+    {tiles = {"B4"}, upper = "dense", lower = "ospaced", tall = 1, freq = 5},
+    {tiles = {"C1"}, upper = "espaced", lower = "espaced", tall = 1, freq = 5},
+    {tiles = {"C2"}, upper = "ospaced", lower = "ospaced", tall = 1, freq = 5},
+    {tiles = {"E1"}, upper = "ospaced", lower = "espaced", tall = 1, freq = 5},
+    {tiles = {"E2"}, upper = "espaced", lower = "ospaced", tall = 1, freq = 5},
+    {tiles = {"F1"}, upper = "dense", lower = "dense", tall = 2, freq = 1},
+    {tiles = {"F2"}, upper = "dense", lower = "dense", tall = 2, freq = 1},
+    {tiles = {"F1", "F2"}, upper = "dense", lower = "dense", tall = 2, freq = 5},
+    {tiles = {"F3"}, upper = "dense", lower = "dense", tall = 2, freq = 5},
+    {tiles = {"F4"}, upper = "dense", lower = "dense", tall = 2, freq = 5},
+    {tiles = {"G1", "G2"}, upper = "dense", lower = "ospaced", tall = 2, freq = 5},
+    {tiles = {"I"}, upper = "espaced", lower = "dense", tall = 1, freq = 4},
+    {tiles = {"J1"}, upper = "ospaced", lower = "ospaced", tall = 2, freq = 1},
+    {tiles = {"J2"}, upper = "ospaced", lower = "ospaced", tall = 2, freq = 1},
+    {tiles = {"J1", "J2"}, upper = "ospaced", lower = "ospaced", tall = 2, freq = 2},
+    {tiles = {"J3"}, upper = "espaced", lower = "espaced", tall = 2, freq = 1},
+    {tiles = {"J4"}, upper = "espaced", lower = "espaced", tall = 2, freq = 1},
+    {tiles = {"J3", "J4"}, upper = "espaced", lower = "espaced", tall = 2, freq = 2},
+    {tiles = {"B1", "I"}, upper = "espaced", lower = "dense", tall = 1, freq = 2}
+}
+
+---The algorithm used to generate SH pattern box, SH grate box, and Alt Grate Box.
+---Adapted from the original Lingo code.
+---@param prefix string The string to prepend to the tile name when placing.
+---@param layer integer The layer to tile.
+---@param left integer The left side of the rectangle.
+---@param top integer The top side of the rectangle.
+---@param right integer The right side of the rectangle.
+---@param bottom integer The bottom side of the rectangle.
+---@param forceModifier ForceModifier
+function module.patternBox(prefix, layer, left, top, right, bottom, forceModifier)
+    -- place corner tiles
+    rained.placeTile("Block Corner NW", layer, left, top, forceModifier)
+    rained.placeTile("Block Corner NE", layer, right, top, forceModifier)
+    rained.placeTile("Block Corner SE", layer, right, bottom, forceModifier)
+    rained.placeTile("Block Corner SW", layer, left, bottom, forceModifier)
+
+    -- fill sides
+    for x = left + 1, right - 1 do
+        rained.placeTile("Block Edge N", layer, x, top, forceModifier)
+        rained.placeTile("Block Edge S", layer, x, bottom, forceModifier)
+    end
+
+    for y = top + 1, bottom - 1 do
+        rained.placeTile("Block Edge W", layer, left, y, forceModifier)
+        rained.placeTile("Block Edge E", layer, right, y, forceModifier)
+    end
+
+    -- the following code is translated straight from the lingo code
+    local py = top + 1
+    local currentPattern = patterns[math.random(#patterns)]
+
+    while py < bottom do
+        local possiblePatterns = {}
+
+        for q=1, #patterns do
+            if patterns[q].upper == currentPattern.lower and py + patterns[q].tall < bottom + 1 then
+                for _=1, patterns[q].freq do
+                    possiblePatterns[#possiblePatterns+1] = q
+                end
+            end
+        end
+
+        currentPattern = patterns[possiblePatterns[math.random(#possiblePatterns)]]
+        local tl = math.random(#currentPattern.tiles)
+
+        for px = left + 1, right - 1 do
+            tl = tl + 1
+            if tl > #currentPattern.tiles then
+                tl = 1
+            end
+
+            rained.placeTile(prefix .. currentPattern.tiles[tl], layer, px, py, forceModifier)
+        end
+
+        py = py + currentPattern.tall
+    end
+end
+
 return module
