7686665217489c2a435d3129f0a3fcd5b01acbe1
pkhead <chromosoze@gmail.com>
2024-05-20 15:50:53 -0500

    input handling

--- src/Glib.Tests/Program.cs
+++ src/Glib.Tests/Program.cs
@@ -5,7 +5,11 @@ namespace GlibTests
 {
     class Program
     {
-        private static Window window;
+        private static Window window = null!;
+        private static float sqX = 0f;
+        private static float sqY = 0f;
+        private static float sqW = 100.0f;
+        private static float sqH = 100.0f;
 
         private static void Main(string[] args)
         {
@@ -14,14 +18,14 @@ namespace GlibTests
             {
                 Width = 800,
                 Height = 600,
-                Title = "Silk.NET test"
+                Title = "Silk.NET test",
             };
             
             window = new Window(options);
 
             // assign events
             window.Load += OnLoad;
-            //window.Update += OnUpdate;
+            window.Update += OnUpdate;
             window.Draw += OnRender;
             //window.Resize += OnResize;
             //window.Closing += OnClose;
@@ -41,11 +45,28 @@ namespace GlibTests
         private static void OnRender(float dt, RenderContext renderContext)
         {
             renderContext.DrawColor = Color.FromArgb(255, 127, 51, 255);
-            renderContext.FillRect(0f, 0f, 100f, 100f);
+            renderContext.FillRect(sqX - sqW / 2.0f, sqY - sqH / 2.0f, sqW, sqH);
         }
 
-        private static void OnUpdate(double obj)
+        private static void OnUpdate(float dt)
         {
+            sqX = window.MouseX;
+            sqY = window.MouseY;
+
+            if (window.IsKeyDown(Key.Escape))
+            {
+                window.Close();
+            }
+
+            if (window.IsKeyPressed(Key.Q))
+            {
+                sqW += 10f;
+            }
+
+            if (window.IsKeyDown(Key.W))
+            {
+                sqH += 60f * dt;
+            }
         }
     }
 }
--- src/Glib/Window.cs
+++ src/Glib/Window.cs
@@ -15,6 +15,21 @@ public class Window : IDisposable
     public event Action<int, int>? Resize;
     public event Action? Closing;
 
+    public event Action<Key, int>? KeyDown;
+    public event Action<Key, int>? KeyUp;
+    public event Action<char>? KeyChar;
+
+    public event Action<float, float>? MouseMove;
+    public event Action<int>? MouseDown;
+    public event Action<int>? MouseUp;
+
+    private Vector2 _mousePos = Vector2.Zero;
+    private readonly List<Key> keyList = []; // The list of currently pressed keys
+    private readonly List<Key> pressList = []; // The list of keys that was pressed on this frame
+
+    public float MouseX { get => _mousePos.X; }
+    public float MouseY { get => _mousePos.Y; }
+
     private RenderContext? _renderContext = null;
     public RenderContext? RenderContext { get => _renderContext; }
 
@@ -29,16 +44,106 @@ public class Window : IDisposable
         window.Closing += OnClose;
     }
 
+    public void Close()
+    {
+        window.Close();
+    }
+
     private void OnLoad()
     {
         IInputContext input = window.CreateInput();
-        /*for (int i = 0; i < input.Keyboards.Count; i++)
+        for (int i = 0; i < input.Keyboards.Count; i++)
         {
-            input.Keyboards[i].KeyDown += KeyDown;
-        }*/
+            input.Keyboards[i].KeyDown += OnKeyDown;
+            input.Keyboards[i].KeyUp += OnKeyUp;
+            input.Keyboards[i].KeyChar += OnKeyChar;
+        }
+
+        if (input.Mice.Count > 0)
+        {
+            var mouse = input.Mice[0];
+            mouse.MouseMove += OnMouseMove;
+            mouse.MouseDown += OnMouseDown;
+            mouse.MouseUp += OnMouseUp;
+        }
 
         _renderContext = new RenderContext(window);
         Load?.Invoke();
+    }
+
+    private void OnKeyChar(IKeyboard keyboard, char @char)
+    {
+        KeyChar?.Invoke(@char);
+    }
+
+    private void OnKeyDown(IKeyboard keyboard, Silk.NET.Input.Key key, int keyCode)
+    {
+        var k = (Key)(int)key;
+
+        if (!keyList.Contains(k))
+            keyList.Add(k);
+        pressList.Add(k);
+        
+        KeyDown?.Invoke(k, keyCode);
+    }
+
+    private void OnKeyUp(IKeyboard keyboard, Silk.NET.Input.Key key, int keyCode)
+    {
+        var k = (Key)(int)key;
+        keyList.Remove(k);
+
+        KeyUp?.Invoke((Key)(int)key, keyCode);
+    }
+
+    private void OnMouseMove(IMouse mouse, Vector2 position)
+    {
+        _mousePos = position;
+        MouseMove?.Invoke(position.X, position.Y);
+    }
+
+    /// <summary>
+    /// Check if a given key is held down.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if the key is down, false if not.</returns>
+    public bool IsKeyDown(Key key) => keyList.Contains(key);
+
+    /// <summary>
+    /// Check if a given key is not held down.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if the key is up, false if not.</returns>
+    public bool IsKeyUp(Key key) => !keyList.Contains(key);
+
+    /// <summary>
+    /// Check if a given key was pressed on this frame.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns>True if the key was pressed on this frame.</returns>
+    public bool IsKeyPressed(Key key) => pressList.Contains(key);
+
+    private static int GetMouseButtonIndex(MouseButton button){
+        return button switch
+        {
+            MouseButton.Left => 0,
+            MouseButton.Right => 1,
+            MouseButton.Middle => 2,
+            _ => -1
+        };
+    }
+
+    private void OnMouseDown(IMouse mouse, MouseButton button)
+    {
+        int intBtn = GetMouseButtonIndex(button);
+        if (intBtn == -1) return;
+        MouseDown?.Invoke(intBtn);
+    }
+
+    private void OnMouseUp(IMouse mouse, MouseButton button)
+    {
+        int intBtn = GetMouseButtonIndex(button);
+        if (intBtn == -1) return;
+        MouseUp?.Invoke(intBtn);
     }
 
     private void OnUpdate(double dt)
@@ -59,6 +164,8 @@ public class Window : IDisposable
         
         Draw?.Invoke((float)dt, _renderContext!);
         _renderContext!.DrawBatch();
+
+        pressList.Clear();
     }
 
     private void OnFramebufferResize(Vector2D<int> newSize)
