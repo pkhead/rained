3dff68ec74ee11d9731b071f30727d691161ca23
pkhead <chromosoze@gmail.com>
2024-07-02 15:32:14 -0500

    update version to 2.0.0-rc1

--- src/Rained/RainEd.cs
+++ src/Rained/RainEd.cs
@@ -21,7 +21,7 @@ public class RainEdStartupException : Exception
 
 sealed class RainEd
 {
-    public const string Version = "b1.5.4";
+    public const string Version = "v2.0.0-rc1";
 
     public static RainEd Instance = null!;
 
