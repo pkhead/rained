81064dea4e99e1a03dc877eaeb2d5f1fec1c0bdc
pkhead <chromosoze@gmail.com>
2024-07-03 16:40:50 -0500

    bump version to v2.0.0

--- src/Rained/RainEd.cs
+++ src/Rained/RainEd.cs
@@ -21,7 +21,7 @@ public class RainEdStartupException : Exception
 
 sealed class RainEd
 {
-    public const string Version = "v2.0.0-rc1";
+    public const string Version = "v2.0.0";
 
     public static RainEd Instance = null!;
 
