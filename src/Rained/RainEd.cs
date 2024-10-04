4f6cc8289b0826e889541eae8acee65e8a2cc410
pkhead <chromosoze@gmail.com>
2024-10-04 11:43:51 -0500

    mark as dev version for nightly build

--- src/Rained/RainEd.cs
+++ src/Rained/RainEd.cs
@@ -21,7 +21,7 @@ public class RainEdStartupException : Exception
 /// </summary>
 sealed class RainEd
 {
-    public const string Version = "v2.1.4";
+    public const string Version = "v2.2.0-dev";
 
     public static RainEd Instance = null!;
 
