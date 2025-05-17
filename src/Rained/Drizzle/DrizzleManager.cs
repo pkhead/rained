c9595cc5cd2b7fe5320ab5894cc7c56466316297
pkhead <chromosoze@gmail.com>
2025-05-17 15:59:41 -0500

    fix editorConfig.txt before running drizzle startup

--- src/Rained/Drizzle/DrizzleManager.cs
+++ src/Rained/Drizzle/DrizzleManager.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Drizzle.Lingo.Runtime;
 using Drizzle.Logic;
 using Drizzle.Ported;
@@ -22,6 +23,79 @@ static class DrizzleManager
     private static LingoRuntime? _staticRuntime;
     public static LingoRuntime? StaticRuntime => _staticRuntime;
 
+    // fix editorConfig.txt, because if invalid the startUp function
+    // will unintentionally abort early, since the movie score is not
+    // implemented in Drizzle.
+    private static void FixEditorConfig(string dataPath)
+    {
+        if (!DrizzleCast.GetFileName("baseConfig.txt", out string? baseConfigPath))
+            throw new Exception("baseConfig.txt not found in cast");
+
+        // create file it does not exist
+        var editorConfigPath = Path.Combine(dataPath, "editorConfig.txt");
+        if (!File.Exists(editorConfigPath))
+        {
+            File.WriteAllText(editorConfigPath, File.ReadAllText(baseConfigPath));
+            return;
+        }
+
+        // version check
+        var editorConfigLines = File.ReadAllLines(editorConfigPath);
+        var baseConfigLines = File.ReadAllLines(baseConfigPath);
+
+        // if version check failed, correct the file
+        if (editorConfigLines[0] != baseConfigLines[0])
+        {
+            // I also want to keep whatever options the user had
+            // so first, I need to read what options existed in the old file
+            Dictionary<string, string> userOptions = [];
+            for (int i = 1; i < editorConfigLines.Length; i++)
+            {
+                var line = editorConfigLines[i];
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var idx = line.IndexOf(" : ");
+                if (idx == -1)
+                {
+                    Log.UserLogger?.Warning("Line {LineNumber} of editorConfig.txt was invalid: {Line}", i+1, line);
+                    continue;
+                }
+
+                var optionName = line[..idx];
+                userOptions[optionName] = line;
+            }
+
+            string[] newLines = new string[baseConfigLines.Length];
+            newLines[0] = baseConfigLines[0];
+
+            for (int i = 1; i < baseConfigLines.Length; i++)
+            {
+                var baseLine = baseConfigLines[i];
+                var idx = baseLine.IndexOf(" : ");
+                if (idx == -1)
+                {
+                    Log.UserLogger?.Warning("Line {LineNumber} of cast baseConfig.txt was invalid: {Line}", i+1, baseLine);
+                    newLines[i] = baseLine;
+                    continue;
+                }
+
+                var optionName = baseLine[..idx];
+
+                if (userOptions.TryGetValue(optionName, out var origLine))
+                {
+                    newLines[i] = origLine;
+                }
+                else
+                {
+                    newLines[i] = baseLine;
+                }
+            }
+
+            // write output to file
+            File.WriteAllLines(editorConfigPath, newLines);
+        }
+    }
+
     private static LingoRuntime CreateRuntime()
     {
         SixLabors.ImageSharp.Configuration.Default.PreferContiguousImageBuffers = true;
@@ -30,6 +104,8 @@ static class DrizzleManager
 
         var runtime = new LingoRuntime(typeof(MovieScript).Assembly);
         runtime.Init();
+
+        FixEditorConfig(LingoRuntime.MovieBasePath);
         EditorRuntimeHelpers.RunStartup(runtime);
 
         return runtime;
